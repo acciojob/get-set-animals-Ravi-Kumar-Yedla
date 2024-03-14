28ce042f03a5f939502bd76b06086d24dcb7628f
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-14 10:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this._species = species;
+	}
+	get species(){
+		return this._species;
+	}
+	makesound(sound){
+		console.log(`${this._species} makes a ${sound} sound`);
+	}
+	
+}
+class Cat extends Animal {
+	constructor(species){
+		super(species)
+	}
+	purr(){
+		console.log("purr");
+	}
+}
 
-class Dog extends Animal {}
 
-class Cat extends Animal {}
+
+class Dog extends Animal {
+	constructor(species){
+		super(species)
+	}
+	bark(){
+		console.log("Woof");
+	}
+}
+
+
 
 // Do not change the code below this line
 window.Animal = Animal;
